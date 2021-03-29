79e14b5b29f071a7e7e22eab1f70f39ced01ce92
준혁 신 <sjh8236@naver.com>
2021-03-29 12:44:29 +0900

    checkbox& radiobox

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,24 +1,62 @@
 import React, {useEffect, useState} from 'react'
 import { FaCode } from "react-icons/fa";
 import axios from "axios";
-import {Icon, Col, Card, Row, Carousel} from 'antd';
+import {Icon, Col, Card, Row, Carousel, Radio} from 'antd';
 import Meta from 'antd/lib/card/Meta';
 import ImageSlider from '../../utils/ImageSlider';
+import Checkbox from './Sections/CheckBox';
+import Radiobox from './Sections/RadioBox';
+import { continents, price } from './Sections/Datas';
 
 function LandingPage() {
     const [Products, setProducts] =useState([])
+    const [Skip, setSkip]= useState(0)
+    const [Limit, setLimit]= useState(8)
+    const [PostSize, setPostSize]=useState(0)
+    const [Filters, setFilters] = useState({
+        continents : [],
+        price: []
+    })
 
     useEffect(() => {
-        axios.post('/api/product/products',)
+
+        let body = {
+            skip: Skip,
+            limit: Limit
+        }
+        getProducts(body)
+        
+    }, [])
+
+    const getProducts = (body) => {
+        axios.post('/api/product/products',body)
             .then(response => {
                 if(response.data.success) {
-
-                    setProducts(response.data.productInfo)
+                    if(body.loadMore){
+                        setProducts([...Products, ...response.data.productInfo])
+                    }else{
+                        setProducts(response.data.productInfo)
+                    }
+                    setPostSize(response.data.postSize)
                 }else {
                     alert("상품들을 가져오는데 실패했습니다.")
                 }
             })
-    }, [])
+    }
+
+    const loadMoreHandler = () => {
+        
+        let skip = Skip +Limit
+
+        let body = {
+            skip: skip,
+            limit: Limit,
+            loadMore:true
+        }
+        
+        getProducts(body)
+        setSkip(skip)
+    }
 
     const renderCards = Products.map((product, index) => {
         return <Col lg={6} md={8} xs={24} key={index}>
@@ -33,6 +71,47 @@ function LandingPage() {
         </Col>
     })
 
+    const showFilteredResults = (filters) => {
+
+        let body ={
+            skip: 0,
+            limit: Limit,
+            filters: filters
+        }
+
+        getProducts(body)
+        setSkip(0)
+
+
+    }
+    const handlePrice = (value) => {
+        const data = price;
+        let array = [];
+
+        for (let key in data) {
+            if(data[key]._id === parseInt(value,10)) {
+                array = data[key].array;
+            }
+        }
+        return array;
+        
+    }
+    const handleFilters = (filters, category) => {
+
+        const newFilters = {...Filters}
+        newFilters[category] = filters
+
+        if(category ==="price"){
+            let priceValues = handlePrice(filters)
+            newFilters[category] = priceValues
+        }
+
+
+        showFilteredResults(newFilters)
+        setFilters(newFilters)
+
+
+    }
 
     return (
        <div style={{width: '75%', margin: '3rem auto'}}>
@@ -41,16 +120,30 @@ function LandingPage() {
            </div>
 
            {/* filter*/ }
+        <Row gutter={[16,16]}>
+            <Col lg={12} xs={24}>
+                {/* CheckBox*/ }
+                <Checkbox list={continents} handleFilters={filters=>handleFilters(filters, "continents")}/>
+            </Col>
+            <Col lg={12} xs={24}>
+                {/* RadioBox*/ }
+                <Radiobox list={price} handleFilters={filters=>handleFilters(filters, "price")}/>
+            </Col>
+        </Row>
+           
+
            {/* search*/ }
            {/* cards*/ }
            <Row gutter={[16, 16]}>
                 {renderCards}
            </Row>
 
-           
-            <div style={{display:'flex', justifyContent: 'center'}}>
-                <button>더보기</button>
-            </div>
+            <br/>
+            {PostSize >= Limit &&
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <button onClick={loadMoreHandler}>더보기</button>
+                </div>
+            }
 
        </div>
     )
--- server/routes/product.js
+++ server/routes/product.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const multer = require('multer');
 const { Product } = require('../models/Product');
 
+
 //=================================
 //             Product
 //=================================
@@ -43,11 +44,39 @@ router.post('/',(req, res) =>{
 
 router.post('/products',(req, res) => {
     // proudct collection에 들어 있는 모든 상품 정보를 가져오기
-    Product.find()
+    let limit = req.body.limit ? parseInt(req.body.limit) :20;
+    let skip = req.body.skip ? parseInt(req.body.skip): 0;
+
+    let findArgs = {};
+
+    for(let key in req.body.filters){
+
+        if(req.body.filters[key].length > 0) {
+
+            if(key==="price") {
+                findArgs[key] = {
+                    $gte:req.body.filters[key][0],
+                    $lte:req.body.filters[key][1]
+                }
+            }else{
+                findArgs[key] = req.body.filters[key];
+
+            }
+
+
+        }
+    }
+
+    Product.find(findArgs)
         .populate("writer")
+        .skip(skip)
+        .limit(limit)
         .exec((err, productInfo) => {
             if(err) return res.status(400).json({success: false, err})
-            return res.status(200).json({success: true, productInfo})
+            return res.status(200).json({
+                success: true, productInfo,
+                postSize: productInfo.length
+            })
         })
 
 
